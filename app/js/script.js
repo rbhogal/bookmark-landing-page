9fa17d589b00e827621a5af8765ce917e928c2fb
rbhogal <rbhogal23gmail.com>
2021-02-18 18:34:43 -0800

    form validation error progress 1

--- app/js/script.js
+++ app/js/script.js
@@ -9,16 +9,22 @@ accItemHeaders.forEach((accItemHeader) => {
     const curActiveAccItemHeader = document.querySelector(
       ".faq__accordion__item__header.active"
     );
+
+    // if there's an active header and it's not the one we clicked, close it
     if (curActiveAccItemHeader && curActiveAccItemHeader !== accItemHeader) {
       curActiveAccItemHeader.classList.toggle("active");
       curActiveAccItemHeader.nextElementSibling.style.maxHeight = 0;
     }
 
+    // Make the clicked header active
     accItemHeader.classList.toggle("active");
     const accItemBody = accItemHeader.nextElementSibling;
 
+    // Determine the height of the body of the active question
     accItemBody.style.maxHeight = accItemHeader.classList.contains("active")
       ? accItemBody.scrollHeight + "px"
       : 0;
   });
 });
+
+
