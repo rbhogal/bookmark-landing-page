0b737f985fcdeb1e5e786574e45e4d0801b74e11
rbhogal <rbhogal23gmail.com>
2021-02-18 19:18:43 -0800

    form validation js progress 2

--- app/js/script.js
+++ app/js/script.js
@@ -3,6 +3,9 @@ const accItemHeaders = document.querySelectorAll(
   ".faq__accordion__item__header"
 );
 
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+
 // faq accordion
 accItemHeaders.forEach((accItemHeader) => {
   accItemHeader.addEventListener("click", () => {
@@ -18,13 +21,36 @@ accItemHeaders.forEach((accItemHeader) => {
 
     // Make the clicked header active
     accItemHeader.classList.toggle("active");
-    const accItemBody = accItemHeader.nextElementSibling;
 
     // Determine the height of the body of the active question
+    const accItemBody = accItemHeader.nextElementSibling;
     accItemBody.style.maxHeight = accItemHeader.classList.contains("active")
       ? accItemBody.scrollHeight + "px"
       : 0;
   });
 });
 
+// form validation error
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkEmailInput();
+});
 
+function checkEmailInput() {
+  const emailValue = email.value.trim();
+
+  if (!isEmail(emailValue)) setError(email);
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
+
+function setError(email) {
+  const formContainer = email.parentElement;
+
+
+  formContainer.classList.toggle("error");
+}
