dab3502318bf19c85f0b61f82cabf02c1d77cc52
rbhogal <rbhogal23gmail.com>
2021-02-19 19:14:48 -0800

    tabs js finished

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,54 @@
+/////////// Feature tabs ///////////
+
+// Selectors
+const tabs = document.querySelectorAll(".features__tab");
+const tabsContainer = document.querySelector(".features__tabs");
+const tabsDivs = document.querySelectorAll(".features__tabs__div");
+const tabsImgs = document.querySelectorAll(".features__tabs__img");
+const tabsContent = document.querySelectorAll(".features__tabs__content");
+
+// 1) Add event listeners
+tabsContainer.addEventListener("click", (e) => {
+  const clicked = e.target;
+  // const tabDiv
+  // const tabImg
+  // const tabContent
+
+  // 2) Remove active classes on tab, div, img, and text contents
+  tabs.forEach((tab) => tab.classList.remove("active"));
+  tabsDivs.forEach((div) => div.classList.remove("active"));
+  tabsImgs.forEach((img) => img.classList.remove("active"));
+  tabsContent.forEach((content) => content.classList.remove("active"));
+
+  // 3) Add active classes to tab, div, img, and content
+
+  // tab
+  clicked.classList.add("active");
+
+  // div
+  document
+    .querySelector(`.features__tabs__divs__div--${clicked.dataset.tab}`)
+    .classList.add("active");
+
+  // img
+  document
+    .querySelector(`.features__tabs__tab-${clicked.dataset.tab}__img`)
+    .classList.add("active");
+
+  // text content
+  document
+    .querySelector(`.features__tabs__tab-${clicked.dataset.tab}__content`)
+    .classList.add("active");
+});
+
+/////////// FAQ accordion ///////////
+
 // Selectors
 const accItemHeaders = document.querySelectorAll(
   ".faq__accordion__item__header"
 );
 
-const form = document.getElementById("form");
-const email = document.getElementById("email");
-
-// Variables
-const formContainer = email.parentElement;
-
-// faq accordion
+// do event delegation instead of for each
 accItemHeaders.forEach((accItemHeader) => {
   accItemHeader.addEventListener("click", () => {
     const curActiveAccItemHeader = document.querySelector(
@@ -33,7 +72,15 @@ accItemHeaders.forEach((accItemHeader) => {
   });
 });
 
-// form validation error
+/////////// Form validation error ///////////
+
+// Selectors
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+
+// Global Variables
+const formContainer = email.parentElement;
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   checkEmailInput();
@@ -47,6 +94,7 @@ function checkEmailInput() {
     formContainer.classList.toggle("error");
   }
 
+  // If email is not valid give error
   if (!isEmail(emailValue)) setError();
 }
 
